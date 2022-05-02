122c0655398c6cbf25de4f2be03c6c7787e849ff
aimee-liang <aimeeeliang@gmail.com>
2022-05-02 13:27:42 -0400

    Removed Chai Arrays package

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const Coder = function(){
     this.languages = ['JavaScript', 'TypeScript', 'Ruby', 'Python']
+    this.frameworks = ['Ruby on Rails', 'Express', 'React']
 }
 
 Coder.prototype.checkLanguages = function() {
     let language = this.languages[Math.floor(Math.random() * this.languages.length)]
     return language
+}
+
+Coder.prototype.checkFrameworks = function(){
+    let framework = this.frameworks[Math.floor(Math.random() * this.frameworks.length)]
+    console.log(`This coder knows ${framework}`)
+    return framework
 }
 
 let coder = new Coder()
--- test/test.js
+++ test/test.js
@@ -1,3 +1,18 @@
 var assert = require('chai').assert
 var expect = require('chai').expect
 var should = require('chai').should()
+
+const Coder  = require('../app')
+
+describe('Coder Languages', function(){
+    let coder = Coder
+
+    it ('check that language is a valid string', function(){
+        assert.isString(coder.checkLanguages(), 'string')
+    })
+
+    it ('check for a language', function(){
+        let language = coder.checkLanguages()
+        assert.oneOf(language, coder.languages)
+    })
+})
